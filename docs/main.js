ea2c96353f11602874affc2d57cf7e63b44836ca
Nex Angelus <2210791@my.ipleiria.pt>
2023-03-19 16:02:23 +0000

    Changed perk selection made page more responsive
    Co-authored-by: André Paulo <19685105+andre-paulo98@users.noreply.github.com>

--- docs/main.js
+++ docs/main.js
@@ -12,10 +12,15 @@ const v = {
             search: '',
             selectedPage: 1,
             currentFocus: -1,
+            choicesElement: null,
+            images: []
         }
     },
     created() {
         this.fetchPerks();
+    },
+    mount() {
+
     },
     computed: {
         pageLimit() {
@@ -40,34 +45,32 @@ const v = {
                 }
                 this.selectedPage = page;
             }
-        }
+        },
+        getPerksSelected: function (val) {
+            if (!this.choicesElement) {
+                this.choicesElement = new Choices("#choices-perks", { searchPlaceholderValue: 'Search for a perk', itemSelectText: '' })
+            }
+            this.choicesElement.clearStore();
+
+            this.choicesElement.setChoices(val.map(e => { return { label: e.name, value: e.name } }));
+
+        },
     },
     methods: {
         fetchPerks() {
             axios.get('data/perks.json').then(res => {
                 this.perks = res.data;
 
-                /*let survivorPerks = [], killerPerks = [];
+                this.survivorPerks = res.data.survivor;
+                this.killerPerks = res.data.killer;
 
-                for (const key in res.data) {
-                    const element = res.data[key];
-                    if (element.role == "survivor") {
-                        survivorPerks.push(element)
-                    } else if (element.role == "killer") {
-                        killerPerks.push(element)
-                    }
-                }
-                console.log(survivorPerks);
-                //order by perk name
-                survivorPerks.sort((a, b) => (a.name > b.name) ? 1 : -1)
-                killerPerks.sort((a, b) => (a.name > b.name) ? 1 : -1)*/
-
-                this.survivorPerks = this.perks.survivor;
-                this.killerPerks = this.perks.killer;
+                this.preloadImage([...res.data.survivor, ...res.data.killer]);
             }).catch(err => console.log(err));
+
         },
         setOptionSelected(optionSelected) {
             this.optionSelected = optionSelected;
+            this.selectedPage = 1;
         },
         getPerksInPage() {
             const page = this.selectedPage - 1;
@@ -75,22 +78,6 @@ const v = {
             const perks = this.getPerksSelected.slice(startIndex, startIndex + 15);
 
             return this.chunkArray(perks, 5);
-        },
-        selectPerk(name) {
-            this.search = name;
-            search.value = name;
-            perksOptions.style.display = 'none';
-            search.style.borderRadius = "5px";
-
-            const text = name.toUpperCase();
-
-            for (let option of perksOptions.options) {
-                if (option.value.toUpperCase() === text) {
-                    option.style.display = "block";
-                } else {
-                    option.style.display = "none";
-                }
-            };
         },
         chunkArray(inputArray, perChunk) {
             const result = inputArray.reduce((resultArray, item, index) => {
@@ -107,62 +94,14 @@ const v = {
 
             return result;
         },
-        searchInput() {
-            var text = search.value.toUpperCase();
-            for (let option of perksOptions.options) {
-                if (option.value.toUpperCase().indexOf(text) > -1) {
-                    option.style.display = "block";
-                } else {
-                    option.style.display = "none";
-                }
+        preloadImage(perks) {
+            for (const perk of perks) {
+                const image = new Image();
+                image.src = 'assets/img/perks/' + perk.image;
+                console.log(image.src)
+                this.images.push(image);
             }
         }
     },
 }
 Vue.createApp(v).mount('#v');
-
-search.onfocus = function () {
-    perksOptions.style.display = 'block';
-    search.style.borderRadius = "5px 5px 0 0";
-};
-
-
-search.onblur = function (e) {
-    perksOptions.style.display = "none";
-    search.style.borderRadius = "5px";
-};
-
-
-var currentFocus = -1;
-search.onkeydown = function (e) {
-    if (e.keyCode == 40) {
-        currentFocus++
-        addActive(perksOptions.options);
-    }
-    else if (e.keyCode == 38) {
-        currentFocus--
-        addActive(perksOptions.options);
-    }
-    else if (e.keyCode == 13) {
-        e.preventDefault();
-        if (currentFocus > -1) {
-            if (perksOptions.options)
-                perksOptions.options[currentFocus].click();
-        }
-    }
-}
-
-function addActive(x) {
-    if (!x) return false;
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0)
-        currentFocus = (x.length - 1);
-    x[currentFocus].classList.add("active");
-}
-function removeActive(x) {
-    for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("active");
-    }
-}
-
